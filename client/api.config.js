c19cfb6c66f8290fbae486f33b52367bce03796f
Henry Han <37286505+PotatoEater0127@users.noreply.github.com>
2018-08-17 17:16:32 -0700

    Update api.config.js

--- client/api.config.js
+++ client/api.config.js
@@ -3,6 +3,6 @@ var cloudName = 'dsiiktj6s';
 
 export default {
   cloudinaryPresetName: 'twoh81zw', //unsigned upload preset
-  cloudinaryKey: '534687556975171',
+  cloudinaryKey: '',
   CloudinaryURL: `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`
 }
