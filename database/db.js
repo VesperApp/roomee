5765d5f9fc19693ff76eb6d01c4f152e71feeb48
e56 <edai28@gmail.com>
2018-10-12 11:12:09 -0400

    heroku start and enviorment variables

--- database/db.js
+++ database/db.js
@@ -1,15 +1,20 @@
 const Sequelize = require('sequelize');
 const { host, dialect, dbName, user, password } = require('./db.config.js');
 // console.log(host);
-const db = new Sequelize(dbName, user, password, {
-  host: host,
-  dialect: dialect,
-  // pool: {
-  //   max: 5,
-  //   min: 1,
-  //   acquire: 30000,
-  //   idle: 1
-  // }
-});
 
-module.exports = db;
+if (process.env.CLEARDB_DATABASE_URL) {
+  const db = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+  module.exports = db;
+} else {
+  const db = new Sequelize(dbName, user, password, {
+    host: host,
+    dialect: dialect,
+    // pool: {
+    //   max: 5,
+    //   min: 1,
+    //   acquire: 30000,
+    //   idle: 1
+    // }
+  });
+  module.exports = db;
+}
