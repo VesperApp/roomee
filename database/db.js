9e380d1f62d01a8c324d07946617797539bd8178
e56 <edai28@gmail.com>
2018-10-20 18:52:09 -0400

    database route edits

--- database/db.js
+++ database/db.js
@@ -7,14 +7,8 @@ if (process.env.CLEARDB_DATABASE_URL) {
   module.exports = db;
 } else {
   const db = new Sequelize(dbName, user, password, {
-    host: host,
-    dialect: dialect,
-    // pool: {
-    //   max: 5,
-    //   min: 1,
-    //   acquire: 30000,
-    //   idle: 1
-    // }
+    host,
+    dialect,
   });
   module.exports = db;
 }
--- database/models/User.js
+++ database/models/User.js
@@ -1,4 +1,3 @@
-const bCrypt = require('bcrypt-nodejs');
 const Sequelize = require('sequelize');
 const db = require('../db');
 
@@ -24,36 +23,5 @@ const User = db.define('user', {
   gender: Sequelize.BOOLEAN,
   age: Sequelize.INTEGER,
 });
-
-User.createUser = (newUser, callback) => {
-  bCrypt.genSalt(14, function(err, salt) {
-    bCrypt.hash(newUser.password, salt, null, (err, hash) => {
-      newUser.password = hash;
-      User.create(newUser)
-        .then(data => callback(null, data))
-        .catch(err => callback(err, null));
-    });
-  });
-};
-
-User.findbyUsername = (username, callback) => {
-  User.findOne({ where: { username } })
-    .then(data => callback(null, data))
-    .catch(err => callback(err, null));
-};
-
-User.validateLogin = (username, password, callback) => {
-  User.findOne({ where: { username } })
-    .then(data =>
-      bCrypt.compare(password, data.password, (err, result) => {
-        if (err) {
-          callback(err, null);
-        } else {
-          callback(null, result ? data.id : false);
-        }
-      })
-    )
-    .catch(err => callback(err, null));
-};
 
 module.exports = User;
