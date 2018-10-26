948ed034ceda0121c32a1a035419ecb0720cfc86
Henry Han <potatoeater0127@gmail.com>
2018-10-26 11:06:34 -0700

    (refactor) Refactor /signup(POST) with passport authentication.

--- server/auth.js
+++ server/auth.js
@@ -1,17 +1,45 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const { validateLogin } = require('../database/databaseRoutes');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { validateLogin, createUser } = require("../database/databaseRoutes");
 
-passport.use(new LocalStrategy( async (username, password, done) => {
-  try {
-    const user = await validateLogin(username, password);
-    if (!user) {
-      return done(null, false, { message: 'Incorrect username or password' });
+passport.use(
+  "local-login",
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await validateLogin(username, password);
+      if (!user) {
+        return done(null, false, { message: "Incorrect username or password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      throw err;
     }
-    return done(null, user);
-  } catch(err) {
-    throw(err);
-  }
-}));
+  })
+);
+
+passport.use(
+  "local-signup",
+  new LocalStrategy(
+    { passReqToCallback: true },
+    async (req, username, password, done) => {
+      const { firstname, lastname, zipCode, gender, age } = req.body;
+      const newUser = {
+        username,
+        password,
+        firstname,
+        lastname,
+        email: username,
+        zipCode,
+        gender,
+        age
+      };
+      const createdUser = await createUser(newUser);
+      if (createdUser) {
+        return done(null, createdUser);
+      }
+      return done(null, false, { message: "username already existed" })
+    }
+  )
+);
 
 module.exports = passport;
