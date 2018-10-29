8f65f9e04d76373bb50236c89c016e1253379968
e56 <edai28@gmail.com>
2018-10-28 22:27:01 -0400

    mvp

--- client/src/searchView.jsx
+++ client/src/searchView.jsx
@@ -2,36 +2,22 @@ import React from 'react';
 import RoomsSearchResultView from './roomsSearchResultView';
 import RoomeesSearchResultView from './unusedComponents/roomeesSearchResultView';
 
-const SearchView = ({ term, listings, roomees, onInput, onSearchRooms, onSearchRoomees, onTitleClick }) => (
+const SearchView = ({ zipcode, listings, roomees, onInput, onSearchRooms, onTitleClick }) => (
   <div>
-    <div className="columns level is-multiline is-mobile is-centered control">
-      <input
-        className="column level-item is-one-quarter input is-small"
-        style={{ textAlign: 'center' }}
-        type="text"
-        onChange={onInput}
-        placeholder="Zipcode"
-      />
-      <button
-        className="button is-small is-success"
-        style={{ textAlign: 'center' }}
-        type="submit"
-        onClick={onSearchRooms}
-      >
-        Search Rooms
-      </button>
-
-      {/* 
-      <button
-        className="button is-small is-info"
-        style={{ textAlign: 'center' }}
-        type="submit"
-        onClick={onSearchRoomees}
-      >
-        Roomees
-      </button> */}
+    <div className="field is-grouped is-grouped-centered">
+      <div className="columns level is-multiline is-mobile is-centered control">
+        <p className="control is-expanded">
+          <input className="input" type="text" onChange={onInput} placeholder="Enter a zipcode" />
+        </p>
+        <p className="control">
+          <button className="button is-info" type="submit" onClick={onSearchRooms}>
+            Search
+          </button>
+        </p>
+      </div>
     </div>
     <div>
+      <br />
       {!listings.length && !roomees.length ? (
         <div className="has-text-centered title is-4">Sorry, no results found in this area</div>
       ) : (
