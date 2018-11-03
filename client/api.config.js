29e92f955e793bf42093769da59c61d73d549a82
Henry Han <potatoeater0127@gmail.com>
2018-11-03 02:27:29 -0700

    (refactor) Seperate out the photo onDrop as an individual function.

--- client/api.config.js
+++ client/api.config.js
@@ -1,7 +1,7 @@
-var cloudName = 'db5rb32ne';
+const cloudName = 'db5rb32ne';
 
-export default {
+module.exports = {
   cloudinaryPresetName: 'sdaclh3g', //unsigned upload preset
   cloudinaryKey: '692822772329959',
-  CloudinaryURL: `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+  cloudinaryURL: `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
 };
